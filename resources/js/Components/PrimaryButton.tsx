6d2d269ef7480b0bf32246e3c3c45cfe2f313817
JastinXyz <jastinlinggar@gmail.com>
2024-12-10 19:55:40 +0700

    feat: ubah warna primary

--- resources/js/Components/PrimaryButton.tsx
+++ resources/js/Components/PrimaryButton.tsx
@@ -14,7 +14,7 @@ export default function PrimaryButton({
     <button
       {...props}
       className={classNames(
-        'inline-flex items-center px-4 py-2 bg-primary-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-primary-700 focus:bg-primary-700 active:bg-primary-900 focus:outline-none focus:ring-2 focus:ring-indigo-300 transition ease-in-out duration-150 justify-center',
+        'inline-flex items-center px-4 py-2 bg-primary-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-primary-700 focus:bg-primary-700 active:bg-primary-900 focus:outline-none focus:ring-2 focus:ring-primary-300 transition ease-in-out duration-150 justify-center',
         props.className,
       )}
     >
