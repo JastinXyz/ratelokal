b4caedaa41b5d18d7bc936a1caed6d3ac30f096b
JastinXyz <jastinlinggar@gmail.com>
2024-12-15 11:45:22 +0700

    cleaning

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,6 @@ Route::get('/search', function () {
     return Inertia::render('Search', ['q' => $q]);
 });
 
-// Route::get('/', function () {
-//     return Inertia::render('Welcome', [
-//         'canLogin' => Route::has('login'),
-//         'canRegister' => Route::has('register'),
-//         'laravelVersion' => Application::VERSION,
-//         'phpVersion' => PHP_VERSION,
-//     ]);
-// });
-
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
